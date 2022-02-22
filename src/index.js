1b137d59ee88a5d896693c14f286e163036457f4
DuyNguyen <duynpps08325@fpt.edu.vn>
2022-02-22 22:03:46 +0700

    22/02/2022 Add template engine to server

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const { engine } = require("express-handlebars")
+const morgan = require('morgan')
+const path = require('path')
+const app = express()
+const port = 3000
+
+// HTTP Logger
+app.use(morgan('combined'))
+
+// Template Engine
+app.engine('hbs', engine({
+    extname: ".hbs",
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resource\\views'))
+
+app.get('/', (req, res) => {
+    console.log(__dirname);
+    res.render("home")
+})
+
+app.get('/news', (req, res) => {
+    res.render("news")
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
