bad928c4d1f15857d7416c765e6fd71ba72aa30e
DuyNguyen <duynpps08325@fpt.edu.vn>
2022-03-08 14:53:38 +0700

    #Commit 08/03/2022: Add url cloud

--- src/app/connection/connection.js
+++ src/app/connection/connection.js
@@ -3,11 +3,11 @@ const { MongoClient } = require('mongodb');
 class Connection {
     constructor() { }
 
-    local = "mongodb://localhost:27017";
-    // cloud = "mongodb+srv://codeudemy:Npd97*93@cluster0.g39g6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    // local = "mongodb://localhost:27017";
+    cloud = "mongodb+srv://codeudemy:Npd97*93@cluster0.g39g6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
     connect(callBack) {
-        MongoClient.connect(this.local, callBack);
+        MongoClient.connect(this.cloud, callBack);
     }
 
 }
