ac5e9aa8113cac19744c7004694ed44be7f4aad9
DuyNguyen <duynpps08325@fpt.edu.vn>
2022-03-10 11:00:01 +0700

    #Commit 10/03/2022: Recheck connect to db v-00.

--- src/app/connection/connection.js
+++ src/app/connection/connection.js
@@ -8,6 +8,11 @@ class Connection {
     url = process.env.DATABASE_URL || "mongodb://localhost:27017";
 
     connect(callBack) {
+        console.log("Url connect to db");
+        console.log(url);
+
+        console.log("Check connect");
+        console.log(MongoClient.connect(this.url, callBack));
         MongoClient.connect(this.url, callBack);
     }
 
