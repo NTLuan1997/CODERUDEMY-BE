cb459915e5ae9b4622911854d66b2a73a7a90da7
DuyNguyen <duynpps08325@fpt.edu.vn>
2022-03-12 13:13:05 +0700

    #Commit 12/03/2022: Add validation form and loader page login

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -1,33 +1,68 @@
 window.onload = function (e) {
     let $ = document.querySelector.bind(document);
+    let $$ = document.querySelectorAll.bind(document);
 
     let url = location.href;
+    let emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let loader = $$(".modal-loader")[0];
+    let email = $("#user_email");
+    let password = $("#user_password");
     let loginForm = $("#user_login");
+
+    (function () {
+        loginForm.setAttribute("disabled", true);
+        password.setAttribute("disabled", true);
+
+        email.addEventListener("keyup", function (e) {
+            let messageEmail = $(`#${this.dataset.id}`);
+            if (!emailRegex.test(this.value)) {
+                messageEmail.style.display = "block";
+                messageEmail.textContent = "Email không đúng vui lòng nhập lại";
+
+            } else {
+                messageEmail.style.display = "none";
+                password.removeAttribute("disabled");
+            }
+        })
+
+        password.addEventListener("keyup", function (e) {
+            let messagePassword = $(`#${this.dataset.id}`);
+            if (this.value.length < 5) {
+                messagePassword.style.display = "block";
+                messagePassword.innerHTML = "Độ dài mật khẩu không đủ";
+
+            } else {
+                messagePassword.style.display = "none";
+                loginForm.removeAttribute("disabled");
+            }
+        })
+
+
+    }());
 
     loginForm.addEventListener("click", function (e) {
         e.preventDefault();
-        let email = $("#user_email").value;
-        let password = $("#user_password").value;
-        let body = { email, password };
+        loader.classList.add("active");
 
-        console.log(body);
-
-        if (email && password) {
+        if (email?.value && password?.value) {
             fetch(url + 'API/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(body)
+                body: JSON.stringify({ "email": email?.value, password: password?.value })
             })
                 .then((res) => {
                     return res.json();
                 })
                 .then((user) => {
                     if (user.status) {
-                        console.log(user);
                         saveUser(user);
+
+                    } else {
+                        setMessage(user, [email, password]);
                     }
+                    loader.classList.remove("active");
                 })
                 .catch((err) => {
                     console.log(err);
@@ -36,7 +71,19 @@ window.onload = function (e) {
     })
 
     function saveUser(user) {
-        localStorage.setItem("user", JSON.stringify(user.data));
+        localStorage.setItem("user", JSON.stringify(user));
         location.href = "/home";
+    }
+
+    function setMessage(data, ...element) {
+        let { status, message, user } = data;
+        let emailMessage = $("#email-feedback");
+        let passwordMessage = $("#password-feedback");
+        emailMessage.style.display = "block";
+        passwordMessage.style.display = "block";
+
+        emailMessage.textContent = message;
+        passwordMessage.textContent = message;
+
     }
 }
--- src/routers/render/newsRouter.js
+++ src/routers/render/newsRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const newsController = require('../../app/controller/newsController');
 
-router.use("/:slug", newsController.detail);
-router.use("/", newsController.index);
+router.get("/:slug", newsController.detail);
+router.get("/", newsController.index);
 
 module.exports = router;
