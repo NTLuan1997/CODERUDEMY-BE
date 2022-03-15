2675b1f90adc66174c954546e69bfec8e1dc0aea
DuyNguyen <duynpps08325@fpt.edu.vn>
2022-03-15 18:24:52 +0700

    #Commit 15/03/2022: Refector font-end system admin v-01

--- src/routers/api/userApi.js
+++ src/routers/api/userApi.js
@@ -4,7 +4,7 @@ const siteController = require("../../app/controller/siteController");
 const userController = require("../../app/controller/userController");
 
 router.get("/home", userController.pageUser);
-router.post("/login", siteController.login);
+router.post("/login", middleware.mapperUserLogin, siteController.login);
 router.post("/new", middleware.mapperUser, userController.newUser);
 router.put("/edit", middleware.mapperUser, userController.editUser);
 router.delete("/remove", middleware.mapperUser, userController.removeUser);
