cd18da3ca68792f7e23ce2209b7553c9830093bc
DuyNguyen <duynpps08325@fpt.edu.vn>
2022-03-17 18:03:27 +0700

    #Commit 17/03/2022: Add function render template

--- src/assets/js/render.js
+++ src/assets/js/render.js
@@ -1,6 +1,16 @@
-export function renderCommonBody(data) {
+export function renderCommonBody(data, hideProperty) {
     let template = data.reduce((accument, curent) => {
         let keys = Object.keys(curent);
+        // let team = keys.map((e) => {
+        //     let test = null;
+        //     hideProperty.forEach((E) => {
+        //         if (e != E) {
+        //             test = e;
+        //         }
+        //     })
+        //     return test;
+        // })
+        // console.log(team);
         return accument.concat("<tr>" +
             keys.reduce((reAccument, reCurrent) => {
                 return reAccument.concat(`<td>${curent[reCurrent]}</td>`);
