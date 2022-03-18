df2ba2a717adb095e1281d1b0ff035fc98afa7ac
DuyNguyen <duynpps08325@fpt.edu.vn>
2022-03-18 14:57:46 +0700

    #Commit 18/03/2022: Refactor method pagination and add method course page.

--- src/assets/js/render.js
+++ src/assets/js/render.js
@@ -1,19 +1,24 @@
+
+import { httpsService } from "./httpService.js";
+let $ = document.querySelector.bind(document);
+let $$ = document.querySelectorAll.bind(document);
 /**
- * 
+ *
+ * Method render table template.
  * @param {*} wrapperTemplate Element HTML box parent arount tbody.
  * @param {*} data object covert to template.
  * @param {*} hideProperty property object not show in template.
  * @param {*} endpoint set up type page VD: ../users, ../course, .../vvv
  */
-export function renderCommonBody(wrapperTemplate, data, hideProperty, endpoint) {
+export function renderBodyTable(wrapperTemplate, data, hideProperty, endPoint) {
     let template = data.reduce((accument, current, curentIndex) => {
-        return accument.concat(renderKeys(current, curentIndex, hideProperty, endpoint, renderTemplate));
+        return accument.concat(renderKeys(current, curentIndex, hideProperty, endPoint, renderTemplate));
     }, []).join("");
 
     wrapperTemplate.innerHTML = template;
 }
 
-function renderKeys(currentData, currentIndex, hideProperty, endpoint, callback) {
+function renderKeys(currentData, currentIndex, hideProperty, endPoint, callback) {
     let keys = Object.keys(currentData).reduce((accument, currunetValue) => {
         if (hideProperty.every((e) => e != currunetValue)) {
             return accument.concat(currunetValue);
@@ -21,28 +26,64 @@ function renderKeys(currentData, currentIndex, hideProperty, endpoint, callback)
         return accument;
     }, []);
 
-    return callback(currentData, currentIndex, keys, endpoint);
+    return callback(currentData, currentIndex, keys, endPoint);
 }
 
-function renderTemplate(currentData, currentIndex, keys, endpoint) {
+function renderTemplate(currentData, currentIndex, keys, endPoint) {
     let template = `<tr> <td>${currentIndex}</td>`;
 
     let content = keys.reduce((accument, currunetValue) => {
         return accument.concat(`<td>${currentData[currunetValue]}</td>`);
     }, []).join("");
 
-    return template += content + renderButtonAction(currentData["_id"], endpoint) + "</tr>";
+    return template += content + renderButtonAction(currentData["_id"], endPoint) + "</tr>";
 }
 
-function renderButtonAction(currentId, endpoint) {
+function renderButtonAction(currentId, endPoint) {
     let template = `
     <td>
         <button type="button" class="btn btn-primary">
-            <a href="/${endpoint}/detail?type=update&id=${currentId}">Sửa</a>
+            <a href="/${endPoint}/detail?type=update&id=${currentId}">Sửa</a>
         </button>
         <button type="button" class="btn btn-danger btn-delete-user" data-toggle="modal"
             data-whatever="${currentId}" data-id="${currentId}" data-target="#deleteUser">Xóa</button>
     </td>`;
 
     return template;
+}
+
+/**
+ * 
+ * Method render pagination template.
+ * @param {*} wrapperTemplate 
+ * @param {*} pageRequire 
+ * @param {*} totalDocument 
+ * @param {*} endPoint 
+ */
+export function renderPagination(wrapperTemplate, pageRequire, totalDocument, endPoint, callBack) {
+    let template = '';
+    let totalPages = Math.ceil(totalDocument / pageRequire);
+    for (let i = 0; i < totalPages; i++) {
+        template += `<li class="page-items" data-id="${(i + 1)}"><a class="page-link">${(i + 1)}</a></li>`;
+    }
+    wrapperTemplate.innerHTML = template;
+    renderPaginationAction(pageRequire, endPoint, callBack)
+}
+
+function renderPaginationAction(pageRequire, endPoint, callBack) {
+    $$(".page-items").forEach(function (item) {
+        item.addEventListener("click", function (e) {
+            let start = pageRequire * (this.dataset.id - 1);
+            httpsService(`${endPoint}?limit=${pageRequire}&start=${start}`, "GET", null)
+                .then((res) => {
+                    return res.json();
+                })
+                .then((data) => {
+                    callBack(data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        })
+    })
 }
--- src/assets/js/user.js
+++ src/assets/js/user.js
@@ -1,30 +1,32 @@
 import { deleteUser } from "./delete.js";
 import { httpsService } from "./httpService.js";
-import { renderCommonBody } from "./render.js";
+import { renderBodyTable, renderPagination } from "./render.js";
 
 window.onload = function (e) {
     const $ = document.querySelector.bind(document);
     const $$ = document.querySelectorAll.bind(document);
 
-    let itemInPage = 5;   // Số phần tử trên page theo yêu cầu khách hàng
-    let totalPage = null; // Số item pagination được tính ra
+    let pageRequire = 5;
+    let wrapperPagination = $$(".pagination")[0];
     let wrapperTemplate = $("#user-table-body");
     let headerTitles = ["STT", "Tên người dùng", "Email", "Password", "Trạng thái", "Tuổi", "Quyền"];
 
     (function () {
-        httpsService(`API/user/home?limit=${itemInPage}&start=0`, "GET", null)
+        httpsService(`API/user/home?limit=${pageRequire}&start=0`, "GET", null)
             .then((res) => {
                 return res.json();
             })
             .then((data) => {
                 renderTableTitle();
-                renderTabPagination(data.length);
-                return data?.users;
+                renderPagination(wrapperPagination, 5, data.length, "API/user/home", (e) => {
+                    renderBodyTable(wrapperTemplate, e?.users, ["_id", "skills", "__v"], "users");
+                    deleteUser($$(".btn-delete-user"));
+                });
+                return data;
             })
             .then((data) => {
-                renderCommonBody(wrapperTemplate, data, ["_id", "skills", "__v"], "users");
+                renderBodyTable(wrapperTemplate, data?.users, ["_id", "skills", "__v"], "users");
                 deleteUser($$(".btn-delete-user"));
-                paginationEvent();
             })
             .catch((err) => {
                 console.log(err);
@@ -38,37 +40,6 @@ window.onload = function (e) {
             return accument.concat(`<th>${current}</th>`);
         }, []).join("");
         header.innerHTML = template;
-    }
-
-    function renderTabPagination(totalItem) {
-        let wrapper = $$(".pagination")[0];
-        totalPage = Math.ceil(totalItem / itemInPage);
-
-        let template = '';
-        for (let i = 0; i < totalPage; i++) {
-            template += `<li class="page-items" data-id="${(i + 1)}"><a class="page-link">${(i + 1)}</a></li>`;
-        }
-        wrapper.innerHTML = template;
-    }
-
-    function paginationEvent() {
-        let items = $$(".page-items");
-        items.forEach((e) => {
-            e.addEventListener("click", function (e) {
-                let start = itemInPage * (this.dataset.id - 1);
-                httpsService(`API/user/home?limit=${itemInPage}&start=${start}`, "GET", null)
-                    .then((res) => {
-                        return res.json();
-                    })
-                    .then((data) => {
-                        renderCommonBody(wrapperTemplate, data?.users, ["_id", "skills", "__v"], "users");
-                        deleteUser($$(".btn-delete-user"));
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    })
-            })
-        })
     }
 
 }
