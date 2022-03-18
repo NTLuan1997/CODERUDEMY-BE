3997daccc6f379eac5ad9d0c9ecaf57cdc2cda98
DuyNguyen <duynpps08325@fpt.edu.vn>
2022-03-18 16:27:32 +0700

    #Commit 18/03/2022: Add information course to page course

--- src/app/controller/courseController.js
+++ src/app/controller/courseController.js
@@ -2,15 +2,7 @@ const courseService = require("../services/courseService");
 
 class CourseController {
 
-    constructor() {
-        // courseService.findOneCourse("6229be63fafe1ed7d304048b")
-        //     .then((data) => {
-        //         console.log(data);
-        //     })
-        //     .catch((err) => {
-        //         console.log(err);
-        //     })
-    }
+    constructor() { }
 
     // Method render template.
     renderCourse(req, res) {
@@ -18,6 +10,18 @@ class CourseController {
     }
 
     // Method support api.
+    pageCourse(req, res) {
+        courseService.findLimitCourse(req.query.limit, req.query.start)
+            .then((data) => {
+                res.status(200).json({
+                    "courses": data[0],
+                    "length": data[1]
+                })
+            })
+            .catch((err) => {
+                throw err;
+            })
+    }
 
 }
 
