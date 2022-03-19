7bff98f445c47ebc0ff42ff073d187bdc78ca394
DuyNguyen <duynpps08325@fpt.edu.vn>
2022-03-19 12:00:05 +0700

    #Commit 19/03/2022: Add multiple value whitelist accept share data from api

--- src/app/middleware/corsMiddleware.js
+++ src/app/middleware/corsMiddleware.js
@@ -1,5 +1,5 @@
 function corsOption(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200", "https://coderudemy.herokuapp.com");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200, https://coderudemy.herokuapp.com");
     res.setHeader("Access-Control-Allow-Methods", "GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
