2fc44ea97e26d7a0d36124044ebc33c316fc12be
DuyNguyen <duynpps08325@fpt.edu.vn>
2022-04-06 15:28:49 +0700

    #Commit 06/04/2022: Remove authentication course - unit - lesson

--- src/routers/api/courseApi.js
+++ src/routers/api/courseApi.js
@@ -6,21 +6,21 @@ const middleware = require("../../app/middleware/courseMiddleware");
 const authen = require("../../app/middleware/authentication");
 
 // COURSE
-router.get("/course", authen.authentication, courseController.course);
+router.get("/course", courseController.course);
 router.post("/course-single", authen.authentication, middleware.courseMapper, courseController.findSingle);
 router.post("/course-new", authen.authentication, authen.permissions, middleware.courseMapper, courseController.newCourse);
 router.put("/course-edit", authen.authentication, authen.permissions, middleware.courseMapper, courseController.editCourse);
 router.delete("/course-remove", authen.authentication, authen.permissions, middleware.courseAcceptRemove, middleware.courseMapper, courseController.removeCourse);
 
 // UNIT
-router.get("/unit", authen.authentication, middleware.unitMapper, unitController.unit);
+router.get("/unit", unitController.unit);
 router.post("/unit-single", authen.authentication, middleware.unitMapper, unitController.findSingleUnit);
 router.post("/unit-new", authen.authentication, authen.permissions, middleware.unitMapper, unitController.newUnit);
 router.put("/unit-edit", authen.authentication, authen.permissions, middleware.unitMapper, unitController.editUnit);
 router.delete("/unit-remove", authen.authentication, authen.permissions, middleware.unitAcceptRemove, middleware.unitMapper, unitController.removeUnit);
 
 // LESSON
-router.get("/lesson", authen.authentication, middleware.lessonMapper, lessonController.lesson);
+router.get("/lesson", lessonController.lesson);
 router.post("/lesson-single", authen.authentication, middleware.lessonMapper, lessonController.findSingleLessson);
 router.post("/lesson-new", authen.authentication, authen.permissions, middleware.lessonMapper, lessonController.newLesson);
 router.put("/lesson-edit", authen.authentication, authen.permissions, middleware.lessonMapper, lessonController.editLesson);
