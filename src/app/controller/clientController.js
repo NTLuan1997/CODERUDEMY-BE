e85cbab8c8c5916517c924223e3b93d94187dd05
DuyNguyen <duynpps08325@fpt.edu.vn>
2022-05-03 12:33:51 +0700

    #Commit 03/05/2022: Add API upload thumbnail.

--- src/app/controller/clientController.js
+++ src/app/controller/clientController.js
@@ -66,6 +66,11 @@ class ClientController {
         })
     }
 
+    Upload(req, res) {
+        console.log(req.body);
+        res.status(200).json({status: true, message: "Upload successful"});
+    }
+
     Delete(req, res) {
         clientService.delete(req.Query)
         .then((data) => {
