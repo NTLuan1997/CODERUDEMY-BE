da16f28c83c0a3dba16e0078c5c531f37aec62cf
DuyNguyen <duynpps08325@fpt.edu.vn>
2022-05-23 10:56:35 +0700

    #Commit 23/05/2022: Refactor API Register account.

--- src/app/controller/client.js
+++ src/app/controller/client.js
@@ -26,14 +26,10 @@ class Client {
             })
         }
 
-        if(req.body.type === "Register") {
-            res.status(200).json({status: true});
+        function save() {
             clientService.save(req.client)
             .then((result) => {
-                return result.token = JWT.generation(result.doc._id);
-            })
-            .then((result) => {
-                res.status(200).json(result);
+                res.status(200).json({status: true, token: JWT.generation(result.doc._id)});
             })
             .catch((err) => {
                 throw err;
@@ -43,6 +39,8 @@ class Client {
         if(req.type === "Edit") { edit() }
 
         if(req.type === "Find") { find() }
+
+        if(req.type === "Register") { save() }
     }
 }
 
