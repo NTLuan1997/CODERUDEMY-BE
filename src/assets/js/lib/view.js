97639f8fea32f09a6a49761597a9baff5d493a9f
DuyNguyen <duynpps08325@fpt.edu.vn>
2022-06-15 16:33:43 +0700

    #Commit 15/06/2022: Add - Refactor Template - Script User - Recycle.

--- src/assets/js/lib/view.js
+++ src/assets/js/lib/view.js
@@ -37,7 +37,7 @@ export class View {
         }, []);
 
         if(type === "Recycle") {
-            template.unshift(`<th><input type="checkbox" class="parent-recycle"></th>`);
+            template.unshift(`<th><input type="checkbox" id="parent-recycle"></th>`);
         }
         template.push("<th>Chúc năng</th>");
 
@@ -55,7 +55,8 @@ export class View {
         let template = [];
         let keys = Object.keys(currentValue);
 
-        if(type === "Recycle") { template.push(`<td><input type="checkbox" class="children-recycle"/></td>`); }
+        if(type === "Recycle") {template.push(`<td><input type="checkbox" data-id="${currentValue._id}" class="childrens-recycle"/></td>`)}
+
         for(let i = 0; i < documentKeys.length; i++) {
             for(let j = 0; j < keys.length; j++) {
                 if(documentKeys[i] === keys[j]) {
@@ -75,7 +76,7 @@ export class View {
             case "Recycle":
                 template = `
                     <td class="d-flex">
-                        <button class="btn-method delete" type="button">Xóa</button>
+                        <button class="btn btn-danger delete" data-id="${token}" type="button">Xóa</button>
                     </td>
                 `;
                 break;
@@ -84,7 +85,7 @@ export class View {
             default:
                 template = `
                     <td class="d-flex">
-                        <a class="btn-method mr-2" href="${page}/detail?type=update&token=${token}">Sửa</a>
+                        <a class="btn-method mr-2" href="${page.pathname}/detail?type=update&token=${token}">Sửa</a>
                         <button class="btn-method delete" data-id="${token}" type="button">Xóa</button>
                     </td>
                 `;
@@ -92,65 +93,5 @@ export class View {
         }
 
         return template;
-    }
-
-
-
-
-    // COMMON TEMPLATE
-    render(Result, ComponentHeader, KeyHeader, ComponentView, KeyComponent) {
-        this.BlankView.classList.remove("active");
-        if(!Result.length) {
-            this.BlankView.classList.add("active");
-
-        } else {
-            this.headerView(ComponentHeader, KeyHeader);
-            let template = Result.reduce((accument, current) => {
-                return accument.concat(`<tr>${this.templateView(current, KeyComponent)}</tr>`);
-            }, []).join(" ");
-            ComponentView.innerHTML = template;
-        }
-    }
-
-    templateView(Result, KeyView) {
-        let template = [];
-        let resultKey = Object.keys(Result);
-        for(let i = 0; i < KeyView.length; i++) {
-            for(let j = 0; j < resultKey.length; j++) {
-                if(KeyView[i] === resultKey[j]) {
-                    template.push(`<td>${Result[resultKey[j]]}</td>`);
-                }
-            }
-        }
-        template.push(this.templateMethod(Result?._id));
-        return template.join(" ");
-    }
-
-    templateMethod(token) {
-        let page = this.Origin.page();
-
-        if(this.Pages.includes(this.Origin.checkTypePage())) {
-            return `
-                <td class="d-flex">
-                    <a class="btn-method mr-2 ${this.Type}" style="cursor: pointer" data-id="${token}">${this.Title}</a>
-                    <a class="btn-method mr-2" href="${page}/detail?type=update&token=${token}">Sửa</a>
-                    <button class="btn-method delete" data-id="${token}" type="button">Xóa</button>
-                </td>
-            `;
-        }
-        return `
-            <td class="d-flex">
-                <a class="btn-method mr-2" href="${page}/detail?type=update&token=${token}">Sửa</a>
-                <button class="btn-method delete" data-id="${token}" type="button">Xóa</button>
-            </td>
-        `;
-    }
-
-    headerView(HeaderView, KeyView) {
-        let template = KeyView.reduce((accument, current) => {
-            return accument.concat(`<th>${current}</th>`);
-
-        }, []).join(" ");
-        HeaderView.innerHTML = template;
     }
 }
