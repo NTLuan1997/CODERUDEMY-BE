be1a3923071401b00ffbe8057bace9f30e2e357e
DuyNguyen <duynpps08325@fpt.edu.vn>
2022-06-30 21:58:49 +0700

    #Commit 30/06/2022: Refactor verify token render template.

--- src/app/controller/web/template.js
+++ src/app/controller/web/template.js
@@ -1,71 +1,76 @@
+const JWT = require("../../utils/jwt");
 class Template {
 
     constructor() { }
 
     render(req, res) {
         if(req.cookies.hasOwnProperty("Authentic")) {
-            switch(req._parsedOriginalUrl.pathname) {
+            if(JWT.verify(req.cookies.Authentic)) {
+                switch(req._parsedOriginalUrl.pathname) {
 
-                case"/web":
-                    res.render("components/web-component", { show: true });
-                    break;
-    
-                case "/web/client":
-                    res.status(200).render('components/client-component/client-component', {show: true});
-                    break;
-    
-                case "/web/client/register":
-                    res.status(200).render('components/client-component/client-register-component', {show: true});
-                    break;
-    
-                case "/web/client/detail":
-                    res.status(200).render('components/client-component/client-detail-component', {show: true});
-                    break;
-    
-                case "/web/course":
-                    res.status(200).render('components/course-component/course-component', {show: true});
-                    break;
-    
-                case "/web/course/detail":
-                    res.status(200).render('components/course-component/course-detail-component', {show: true});
-                    break;
-    
-                case "/web/course/unit":
-                    res.status(200).render('components/course-component/unit-component/unit-component', {show: true});
-                    break;
-    
-                case "/web/course/unit/detail":
-                    res.status(200).render('components/course-component/unit-component/unit-detail-component', {show: true});
-                    break;
-    
-                case "/web/course/unit/lesson":
-                    res.status(200).render('components/course-component/lesson-component/lesson-component', {show: true});
-                    break;
-    
-                case "/web/course/unit/lesson/detail":
-                    res.status(200).render('components/course-component/lesson-component/lesson-detail-component', {show: true});
-                    break;
-    
-                case "/web/user":
-                    res.status(200).render("components/user-component/user-component", {show: true});
-                    break;
-    
-                case "/web/user/detail":
-                    res.status(200).render("components/user-component/user-detail-component", {show: true});
-                    break;
-    
-                case "/web/client/recycle":
-                case "/web/course/unit/lesson/recycle":
-                case "/web/course/unit/recycle":
-                case "/web/course/recycle":
-                case "/web/user/recycle":
-                    res.status(200).render("components/common-component/recycle-component", {show: true});
-                    break;
-    
-                case"/":
-                default:
-                    res.render("components/user-component/user-signin-component", { show: false });
-                    break;
+                    case"/web":
+                        res.render("components/web-component", { show: true });
+                        break;
+        
+                    case "/web/client":
+                        res.status(200).render('components/client-component/client-component', {show: true});
+                        break;
+        
+                    case "/web/client/register":
+                        res.status(200).render('components/client-component/client-register-component', {show: true});
+                        break;
+        
+                    case "/web/client/detail":
+                        res.status(200).render('components/client-component/client-detail-component', {show: true});
+                        break;
+        
+                    case "/web/course":
+                        res.status(200).render('components/course-component/course-component', {show: true});
+                        break;
+        
+                    case "/web/course/detail":
+                        res.status(200).render('components/course-component/course-detail-component', {show: true});
+                        break;
+        
+                    case "/web/course/unit":
+                        res.status(200).render('components/course-component/unit-component/unit-component', {show: true});
+                        break;
+        
+                    case "/web/course/unit/detail":
+                        res.status(200).render('components/course-component/unit-component/unit-detail-component', {show: true});
+                        break;
+        
+                    case "/web/course/unit/lesson":
+                        res.status(200).render('components/course-component/lesson-component/lesson-component', {show: true});
+                        break;
+        
+                    case "/web/course/unit/lesson/detail":
+                        res.status(200).render('components/course-component/lesson-component/lesson-detail-component', {show: true});
+                        break;
+        
+                    case "/web/user":
+                        res.status(200).render("components/user-component/user-component", {show: true});
+                        break;
+        
+                    case "/web/user/detail":
+                        res.status(200).render("components/user-component/user-detail-component", {show: true});
+                        break;
+        
+                    case "/web/client/recycle":
+                    case "/web/course/unit/lesson/recycle":
+                    case "/web/course/unit/recycle":
+                    case "/web/course/recycle":
+                    case "/web/user/recycle":
+                        res.status(200).render("components/common-component/recycle-component", {show: true});
+                        break;
+        
+                    case"/":
+                    default:
+                        res.render("components/user-component/user-signin-component", { show: false });
+                        break;
+                }
+            } else {
+                res.render("components/user-component/user-signin-component", { show: false });
             }
         } else {
             res.render("components/user-component/user-signin-component", { show: false });
